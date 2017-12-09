088caa86477e2e6ab2f031dbb7cbcd08f56565f0
王 <513972422@qq.com>
2017-12-09 11:40:34 +0800

    修改注释说明
    注释声明要转换的二维数组必须是有序从0开始的二维数组

--- cartesianProduct.php
+++ cartesianProduct.php
@@ -6,7 +6,7 @@
 
 /**
  * 笛卡尔乘积-递归版
- * @param $arr 要转换成笛卡尔乘积的二维数组
+ * @param $arr 要转换成笛卡尔乘积的二维数组 使用之前请用array_values()对数组进行序列化（0 1 2 3）数组下标必须有序
  * @param int $depth 用于递归的层级
  * @param array $_arr 用于递归数据
  * @param array $returnArr 用于递归数据
